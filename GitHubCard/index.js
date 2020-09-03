08b575b2b21ccfcb686a7d15b1a48ab6ce94621b
LBU <lbu@LBUui-MacBook-Pro.local>
2020-09-03 15:13:25 -0400

    initial commit

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,12 @@
+import axios from 'axios';
+
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/lbu0413')
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
